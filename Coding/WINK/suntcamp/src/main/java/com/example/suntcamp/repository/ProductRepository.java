022fa04a8570d712e9ca6e2ece86fa1e7404d161
didskfo <didskfo2003@gmail.com>
2025-08-07 14:24:46 +0900

    CHORE: 잘못된 폴더 삭제

--- Coding/WINK/suntcamp/src/main/java/com/example/suntcamp/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.suntcamp.repository;
-
-import com.example.suntcamp.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
